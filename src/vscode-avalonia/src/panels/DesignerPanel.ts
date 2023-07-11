78d05916c7d9fefed52729308d87c8fd9a5d46bc
Prashant Cholachagudda <pvc@outlook.com>
2023-07-11 13:46:54 +0530

    Adding unstaged files from previous commit 🙄

--- src/vscode-avalonia/src/panels/DesignerPanel.ts
+++ src/vscode-avalonia/src/panels/DesignerPanel.ts
@@ -115,9 +115,7 @@ export class DesignerPanel {
       <html lang="en">
         <head>
           <meta charset="utf-8">
-          <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-        //   <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
-          <link rel="stylesheet" type="text/css" href="${stylesUri}">
+          <link rel="stylesheet" type="text/css" href="${stylesUri}" nonce="${nonce}">
           <title>Hello World</title>
         </head>
         <body>
