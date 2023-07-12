3d8a6970daa27228bd94bc69f33d88829c142f93
Prashant Cholachagudda <pvc@outlook.com>
2023-07-12 21:20:19 +0530

    Update the version and build script

--- .vscode/package.sh
+++ .vscode/package.sh
@@ -1,50 +1,26 @@
 #!/bin/bash
 
-# Print current path
-echo "Current path: $(pwd)"
-
-# Get path to dotnet executable
-DOTNET_PATH=$(which dotnet)
-
-# Check if dotnet is installed
-if [ -z "$DOTNET_PATH" ]
-  then
-    echo "dotnet not found"
-    exit 1
-fi
-
-# Print project path
-echo "project path to $1"
-
-# Remove output directory
-rm -rf "$2"
-
-$DOTNET_PATH build "$1" -c Release \
-    /property:GenerateFullPaths=true \
-    /consoleloggerparameters:NoSummary \
-    --output "$2"
-
 # Copy LICENSE file to local directory
 cp ../../LICENSE .
 
 # Check if output path is provided
-if [ -z "$3" ]
+if [ -z "$1" ]
   then
     echo "Output path not provided"
     vsce package
 else
     # Create output directory if it doesn't exist
-    if [ ! -d "$3" ]
+    if [ ! -d "$1" ]
     then
-        echo "Creating output directory: $3"
-        mkdir -p "$3"
+        echo "Creating output directory: $1"
+        mkdir -p "$1"
     fi
 
     # Print output path
-    echo "Packaging extension to $3"
+    echo "Packaging extension to $1"
 
     # Package the extension
-    vsce package -o "$3"
+    vsce package -o "$1"
 fi
 
 # Remove LICENSE file from local directory
