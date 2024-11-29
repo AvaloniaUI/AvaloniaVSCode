b6d925be61fb54659497307b75a7b90969f92008
SignalMQ <matnazarovsobirjon123@gmail.com>
2024-11-29 23:33:43 +0500

    Added scale buttons

--- src/vscode-avalonia/src/panels/WebPreviwerPanel.ts
+++ src/vscode-avalonia/src/panels/WebPreviwerPanel.ts
@@ -94,18 +94,77 @@ export class WebPreviewerPanel {
 	private _getHtmlForWebview(url: string) {
 		return `
 		<!DOCTYPE html>
-        <html>
-        <head>
-            <meta charset="utf-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Web Previewer</title>
-            <style>
-                .background {display: flex; justify-content: center; background-size: 15px 15px; background-image: linear-gradient(to right, var(--vscode-focusBorder) 0.1px, transparent 1px), linear-gradient(to bottom, var(--vscode-focusBorder) 0.1px, transparent 1px);}
-            </style>
-        </head>
-        <body class="background">
-            <iframe class="preview" src="${url}" style="width:100%; height:100vh;" frameborder="0"/>
-        </body>
-        </html>`;
+		<html>
+
+		<head>
+			<meta charset="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Web Previewer</title>
+			<style>
+				html,
+				body,
+				iframe {
+					margin: 0;
+					padding: 0;
+				}
+
+				body {
+					background-size: 15px 15px;
+					background-image: linear-gradient(to right, var(--vscode-focusBorder) 0.1px, transparent 1px), linear-gradient(to bottom, var(--vscode-focusBorder) 0.1px, transparent 1px);
+				}
+
+				button {
+					transition-duration: 0.2s;
+					border-radius: 100px;
+					border: none;
+					background: var(--vscode-button-background);
+					foreground: var(--vscode-button-foreground);
+					min-width: 22px;
+					font-size: 18px;
+				}
+				button:hover {
+					background: var(--vscode-button-hoverBackground);
+				}
+
+				#menubar {
+					background: var(--vscode-tab-activeBackground);
+					padding: 5px;
+				}
+
+				#preview {
+					position: fixed;
+					height: 100%;
+					width: 100%;
+				}
+			</style>
+		</head>
+
+		<body>
+			<div id="menubar">
+				<button onclick="Scale(-1)">-</button>
+				<button onclick="Scale(1)">+</button>
+				<button onclick="Scale(0)" id="scaleBtn"></button>
+			</div>
+			<iframe src="${url}" frameborder="0" id="preview"></iframe>
+			<script>
+				var preview = document.getElementById('preview');
+				var scaleBtn = document.getElementById('scaleBtn');
+				var scale = 1.0;
+
+				scaleBtn.textContent = scale;
+
+				function Scale(direction) {
+					if (direction == -1 && scale > 0.25) scale -= 0.25;
+					if (direction == 0) scale = 1.0;
+					if (direction == 1 && scale < 2) scale += 0.25;
+
+					scaleBtn.textContent = scale;
+					preview.style.transformOrigin = 'top left';
+					preview.style.scale = scale;
+				}
+			</script>
+		</body>
+
+		</html>`;
 	}
 }
